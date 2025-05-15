0d3f3fb235daccccbfbb90f212bf7a390b0f69d8
Shree <107465077+s-meher@users.noreply.github.com>
2025-05-15 12:37:13 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,26 @@
-function getBotResponse(input) {
-  return fetch("https://smeher.pythonanywhere.com/chat", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ message: input })
-  })
-    .then(res => res.json())
-    .then(data => data.response)
-    .catch(() => "Oops! Server error.");
-}
-
-async function sendMessage() {
+function sendMessage() {
   const input = document.getElementById("user-input").value;
   const chatBox = document.getElementById("chat-box");
 
-  chatBox.innerHTML += `<p><strong>You:</strong> ${input}</p>`;
+  const userMessage = `<p><strong>You:</strong> ${input}</p>`;
+  chatBox.innerHTML += userMessage;
 
-  const botReply = await getBotResponse(input);
-  chatBox.innerHTML += `<p><strong>Bot:</strong> ${botReply}</p>`;
+  fetch("https://smeher.pythonanywhere.com/chat", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ message: input })
+  })
+    .then(response => response.json())
+    .then(data => {
+      const botReply = `<p><strong>Bot:</strong> ${data.response}</p>`;
+      chatBox.innerHTML += botReply;
+      chatBox.scrollTop = chatBox.scrollHeight;
+    })
+    .catch(error => {
+      chatBox.innerHTML += `<p><strong>Bot:</strong> Oops! Server error.</p>`;
+    });
 
   document.getElementById("user-input").value = "";
-  chatBox.scrollTop = chatBox.scrollHeight;
 }
